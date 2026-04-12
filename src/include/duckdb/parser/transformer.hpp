ab659a6082ac478f872da1e2d3b2cd83ce22df82
Pavel && Konstantin Vedernikoffs <pavel.ivanov@serenedb.com>
2026-04-12 18:02:47 +0000

    feat: PG errors for default / generated cols

--- src/include/duckdb/parser/transformer.hpp
+++ src/include/duckdb/parser/transformer.hpp
@@ -278,7 +278,7 @@ private:
 	//===--------------------------------------------------------------------===//
 	unique_ptr<Constraint> TransformConstraint(duckdb_libpgquery::PGConstraint &constraint);
 	unique_ptr<Constraint> TransformConstraint(duckdb_libpgquery::PGConstraint &constraint, ColumnDefinition &column,
-	                                           idx_t index);
+	                                           idx_t index, const string &table_name = "");
 
 	//===--------------------------------------------------------------------===//
 	// Update transform
