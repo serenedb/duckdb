8451ba929d04ca624b7ecee1561872fa388aa5aa
mbkkt <mbkkt@users.noreply.github.com>
2026-04-17 18:59:09 +0200

    Fix

--- src/parser/transform/statement/transform_drop.cpp
+++ src/parser/transform/statement/transform_drop.cpp
@@ -61,6 +61,10 @@ unique_ptr<SQLStatement> Transformer::TransformDrop(duckdb_libpgquery::PGDropStm
 	case duckdb_libpgquery::PG_OBJECT_FUNCTION:
 		info.type = CatalogType::MACRO_ENTRY;
 		break;
+	case duckdb_libpgquery::PG_OBJECT_PROCEDURE:
+		info.type = CatalogType::TABLE_MACRO_ENTRY;
+		info.is_procedure = true;
+		break;
 	case duckdb_libpgquery::PG_OBJECT_TABLE_MACRO:
 		info.type = CatalogType::TABLE_MACRO_ENTRY;
 		break;
