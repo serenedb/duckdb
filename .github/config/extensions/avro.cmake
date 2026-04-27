ce39310e9e2f928601fa2d4aa585d1a3b6c2d8e1
mbkkt <mbkkt@users.noreply.github.com>
2026-04-27 14:32:36 +0200

    Fix

--- .github/config/extensions/avro.cmake
+++ .github/config/extensions/avro.cmake
@@ -1,8 +1,6 @@
 if (NOT MINGW)
     duckdb_extension_load(avro
-            LOAD_TESTS
-            GIT_URL https://github.com/duckdb/duckdb-avro
-            GIT_TAG 4fa0f73f816e9878d5b6a39795e060877766ac1a
-            APPLY_PATCHES
+            SOURCE_DIR ${CMAKE_SOURCE_DIR}/third_party/duckdb_avro
+            INCLUDE_DIR ${CMAKE_SOURCE_DIR}/third_party/duckdb_avro/src/include
     )
 endif()
