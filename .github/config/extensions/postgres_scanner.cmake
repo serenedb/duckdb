100b27177a23ca599e370d73d25096063d6b4c12
gnusi <andrey@serenedb.com>
2026-05-24 00:26:12 +0200

    Fix postgres scanner

--- .github/config/extensions/postgres_scanner.cmake
+++ .github/config/extensions/postgres_scanner.cmake
@@ -2,9 +2,6 @@
 #       uses a remote rds server but that's not something we want to run here.
 if (NOT MINGW AND NOT ${WASM_ENABLED})
     duckdb_extension_load(postgres_scanner
-            DONT_LINK
-            GIT_URL https://github.com/duckdb/duckdb-postgres
-            GIT_TAG a42c490df0019406658073c003b7d89dd4338466
-            APPLY_PATCHES
+            SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/../duckdb_postgres
             )
 endif()
