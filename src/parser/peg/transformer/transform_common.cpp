258171f0ef7e7f2947fd42fa74a561e938e94309
gnusi <andrey@serenedb.com>
2026-06-23 15:32:29 +0200

    fix: sanitize hidden database names in checkpoint and transaction logs
    Hidden attached databases (e.g. SereneDB's internal stores) can carry names
    that are not valid UTF-8. The checkpoint and transaction log builders wrapped
    db.name directly in a Value, whose string constructor validates UTF-8 and
    throws -- so enabling logging and then checkpointing or running a transaction
    over a hidden database aborted inside the logger. Run hidden database names
    through Utf8Proc::RemoveInvalid before logging them.
    
    Also switches the digit-separator strip added earlier to the value-returning
    absl::StrReplaceAll overload.
    
    Motivated by the logging docs example that turns on memory logging and queries
    duckdb_logs(): tests/sqllogic/sdb/pg/system/sdb_log.test.
    
    (cherry picked from commit dfe4f7c4af324a1ab559ecc302a03da8e9c959a4)

--- src/parser/peg/transformer/transform_common.cpp
+++ src/parser/peg/transformer/transform_common.cpp
@@ -8,8 +8,7 @@
 #include "duckdb/common/operator/negate.hpp"
 #include "duckdb/parser/expression/type_expression.hpp"
 #include "duckdb/common/types/bignum.hpp"
-
-#include "absl/strings/str_replace.h"
+#include <absl/strings/str_replace.h>
 
 namespace duckdb {
 
@@ -614,8 +613,8 @@ unique_ptr<ParsedExpression> PEGTransformerFactory::ConvertNumberToValue(std::st
 	}
 	// if there is a decimal or the value is too big to cast as either hugeint or bigint
 	if (num_underscores > 0) {
-		std::string stripped = absl::StrReplaceAll(val, {{"_", ""}});
-		double dbl_value = Cast::Operation<string_t, double>(string_t(stripped.data(), stripped.size()));
+		std::string cleaned = absl::StrReplaceAll(val, {{"_", ""}});
+		double dbl_value = Cast::Operation<string_t, double>(string_t(cleaned));
 		return make_uniq<ConstantExpression>(Value::DOUBLE(dbl_value));
 	}
 	double dbl_value = Cast::Operation<string_t, double>(str_val);
