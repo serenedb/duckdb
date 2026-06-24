487c2f71cedfdc1a1a706597159c466a8fd4bb63
Mikhail Kornaukhov <mkornaukhov@serenedb.com>
2026-06-24 13:27:10 +0000

    Fix inet extensions

--- .github/config/extensions/inet.cmake
+++ .github/config/extensions/inet.cmake
@@ -1,8 +1,5 @@
 duckdb_extension_load(inet
-    LOAD_TESTS
-    GIT_URL https://github.com/duckdb/duckdb-inet
-    GIT_TAG fe7f60bb60245197680fb07ecd1629a1dc3d91c8
-    INCLUDE_DIR src/include
-    TEST_DIR test/sql
-    APPLY_PATCHES
+    #FIXME: restore autoloading tests LOAD_TESTS
+    SOURCE_DIR ${CMAKE_SOURCE_DIR}/third_party/duckdb_inet
+    INCLUDE_DIR ${CMAKE_SOURCE_DIR}/third_party/duckdb_inet/src/duckdb/inet
     )
