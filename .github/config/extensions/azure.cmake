316d500a3add862052a3f94ce8a3340d0facf362
Mikhail Kornaukhov <mkornaukhov@serenedb.com>
2026-08-03 17:07:13 +0000

    internal-94: Build azure extension from the serenedb in-tree submodule

--- .github/config/extensions/azure.cmake
+++ .github/config/extensions/azure.cmake
@@ -1,8 +1,7 @@
 if (NOT MINGW AND NOT ${WASM_ENABLED})
   duckdb_extension_load(azure
         LOAD_TESTS
-        GIT_URL https://github.com/duckdb/duckdb-azure
-        GIT_TAG abea9d0d90a1f585c3be41d07f725683fdaa0832
-        APPLY_PATCHES
+        SOURCE_DIR ${CMAKE_SOURCE_DIR}/third_party/duckdb_azure
+        INCLUDE_DIR ${CMAKE_SOURCE_DIR}/third_party/duckdb_azure/src/include
   )
 endif()
