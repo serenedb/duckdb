8a7005440d4ba2f2199483378973f01439060f85
Mikhail Kornaukhov <mkornaukhov@serenedb.com>
2026-08-28 16:50:56 +0200

    feat: vendor duckdb_spatial

--- .github/config/extensions/spatial.cmake
+++ .github/config/extensions/spatial.cmake
@@ -1,11 +1,8 @@
-if (${BUILD_COMPLETE_EXTENSION_SET})
-################# SPATIAL
-duckdb_extension_load(spatial
-    DONT_LINK LOAD_TESTS
-    GIT_URL https://github.com/duckdb/duckdb-spatial
-    GIT_TAG 6019a321b4e9a91cb0e81e29d8458d41c75ec3ba
-    INCLUDE_DIR src/spatial
-    TEST_DIR test/sql
-    APPLY_PATCHES
-    )
+if (NOT MINGW AND NOT ${WASM_ENABLED})
+    duckdb_extension_load(spatial
+            SOURCE_DIR ${CMAKE_SOURCE_DIR}/third_party/duckdb_spatial
+            INCLUDE_DIR ${CMAKE_SOURCE_DIR}/third_party/duckdb_spatial/src/spatial
+            LOAD_TESTS
+            TEST_DIR ${CMAKE_SOURCE_DIR}/third_party/duckdb_spatial/test/sql
+            )
 endif()
