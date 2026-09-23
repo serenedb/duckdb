4b6e5efdcf7267b84c99386fe096e6a19b94d621
Pavel && Konstantin Vedernikoffs <pavel.ivanov@serenedb.com>
2026-09-23 08:14:08 +0000

    Revert "Catalog-split fork fixes: error subtypes, keyword folding, detach-wait re-attach"
    This reverts commit 17b06b892bb6510a80b2d33b66bd4c393c289719.

--- src/transaction/meta_transaction.cpp
+++ src/transaction/meta_transaction.cpp
@@ -328,8 +328,7 @@ vector<shared_ptr<AttachedDatabase>> &MetaTransaction::GetStatementDatabases(Cli
 
 void MetaTransaction::ModifyDatabase(AttachedDatabase &db, DatabaseModificationType modification) {
 	if (IsReadOnly()) {
-		throw TransactionException(Exception::InitializeExtraInfo("READ_ONLY", optional_idx()),
-		                           "Cannot write to database \"%s\" - transaction is launched in read-only mode",
+		throw TransactionException("Cannot write to database \"%s\" - transaction is launched in read-only mode",
 		                           db.GetName());
 	}
 	auto &transaction = GetTransaction(db);
@@ -357,7 +356,6 @@ void MetaTransaction::ModifyDatabase(AttachedDatabase &db, DatabaseModificationT
 	}
 	if (&db != modified_database.get()) {
 		throw TransactionException(
-		    Exception::InitializeExtraInfo("CROSS_DATABASE_WRITE", optional_idx()),
 		    "Attempting to write to database \"%s\" in a transaction that has already modified database \"%s\" - a "
 		    "single transaction can only write to a single attached database.",
 		    db.GetName(), modified_database->GetName());
